9686f7ce57e2fec56b6ef324b82122c075ea6d60
Donal Mee <mee.donal@gmail.com>
2021-09-22 13:15:27 +0300

    provide user repeat/exit/continue
    Cannot auto-continue easily because waiting for line input is a blocking
    call without a built-in timeout. So user must hit enter for next phrase.

--- plyanging/ModeListen.py
+++ plyanging/ModeListen.py
@@ -29,19 +29,57 @@ class ModeListen:
         self.phrases = phrases
         self.sound_directory = sound_directory
 
-    def cmdline(self):
+    def _user_wants_next_phrase(self) -> bool:
+        """Presents user with choice whether wants to repeat the current
+        phrase or continue.
+
+        Returns a boolean value. True indicates user wants the next phrase.
+        False indicates they want the current phrase to be repeated.
+        May also exit the program per user choice.
+        """
+        print('(R)epeat. (C)ontinue. (E)xit. \n'
+              'Press enter after your selection. Enter = Continue\n')
+        response = input()
+
+        if 'r' == response:
+            next_phrase = False
+        elif 'c' == response or '' == response:
+            next_phrase = True
+        elif 'e' == response:  # user wants to exit the program.
+            # yes e in in repeat and continue, but by this branch e
+            # can only mean exit.
+            exit(0)
+        # else keep looking for input from the user.
+        else:
+            return self._user_wants_next_phrase()
+
+        return next_phrase
+
+    def _play_english_german(self, phrase:Phrase, count:int) -> None:
+        print('-' * 20)  # mark beginning of phrase
+        print('Phrase Number {0}'.format(count))
+        play_sound = PlaySound(phrase=phrase, sound_directory=self.sound_directory)
+        # play english sound once.
+        print('English: {0}'.format(phrase.english_text))
+        play_sound.english_voice()
+        time.sleep(2)
+        # play german sound twice.
+        print('German: {0}'.format(phrase.german_text))
+        play_sound.german_voice()
+        time.sleep(2)
+        play_sound.german_voice()
+        print('-' * 20)  # mark end of phrase
+
+    def _phrase_loop(self, phrase:Phrase, count:int) -> None:
+        while True:
+            self._play_english_german(phrase=phrase, count=count)
+            next_phrase = self._user_wants_next_phrase()
+            if next_phrase:
+                break
+            else:
+                continue
+
+    def cmdline(self) -> None:
         """Run ModeListen in cmdline mode."""
-        for phrase in self.phrases():
-            play_sound = PlaySound(phrase=phrase, sound_directory=self.sound_directory)
-            # play english sound once.
-            print('English: {0}'.format(phrase.english_text))
-            play_sound.english_voice()
-            time.sleep(2)
-            # play german sound twice.
-            print('German: {0}'.format(phrase.german_text))
-            play_sound.german_voice()
-            time.sleep(2)
-            play_sound.german_voice()
-            # prepare for next phrase
-            print('-'*20)
-            time.sleep(2)
+        for count, phrase in enumerate(self.phrases()):
+            self._phrase_loop(phrase=phrase, count=count)
