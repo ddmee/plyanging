e37e884af2314cd91fddce94ec60b452792cc4bd
Donal Mee <mee.donal@gmail.com>
2022-01-05 10:22:05 +0000

    react - messing around with react

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -6,10 +6,37 @@ import Clock from './Clock';
 import reportWebVitals from './reportWebVitals';
 
 
+function Welcome(props: any) {
+  return <h1>hello {props.name} </h1>;
+}
+
+// Get a list of books available to read. Display as a clickable list
+// Click on the book, navigates to the text of the book.
+
+function BookList(props: any) {
+  const books = props.books;
+  const bookList = books.map((book:string) =>
+    <li key={book}>
+      {book}
+    </li>
+  );
+  return (
+    <ul>{bookList}</ul>
+  );
+}
+
+
+// load the list of books from the server api route
+// make a get request to the book get endpoint
+const books = ['The Last Kingdom', 'Things a Little Bird Told Me', 'Marked for Death'];
+
+
 function tick() {
   ReactDOM.render(
     <React.StrictMode>
       <Clock />
+      <Welcome name='roboto' />
+      <BookList books={books} />
     </React.StrictMode>,
     document.getElementById('root')
   );
